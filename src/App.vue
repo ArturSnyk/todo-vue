d3c19a7d51ece3650446384e81a855716790dc7f
Artur <artur@snyk.io>
2020-02-26 21:50:28 +0200

    add Todos

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,44 @@
 <template>
   <div id="app">
     <HelloWorld msg="Hello World!" />
+    <!--
+     First todos is passed as a prop
+     the second one is the data we pass
+    -->
+    <Todos v-bind:todos="todos" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from "./components/HelloWorld";
+import Todos from "./components/Todos";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    HelloWorld,
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Todo one",
+          compleate: false
+        },
+        {
+          id: 2,
+          title: "Todo two",
+          compleate: true
+        },
+        {
+          id: 3,
+          title: "Todo three",
+          compleate: false
+        }
+      ]
+    };
   }
 };
 </script>
---
+++ src/components/Todos.vue
@@ -0,0 +1,19 @@
+/* eslint-disable vue/valid-template-root */
+<template>
+  <div>
+    <div v-bind:key="todo.id" v-for="todo in todos">
+      <h2>{{todo.title}}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Todo",
+  props: ["todos"]
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
